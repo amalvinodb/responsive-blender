898ddbf5821a92782620b393a6179d4d071b8352
unknown <amalvinodb2000@gmail.com>
2022-08-12 10:09:28 +0530

    some changes

--- scrypt.js
+++ scrypt.js
@@ -1,14 +1,14 @@
-const ham = document.querySelector('.burger');
-const navi = document.querySelector('.navitem');
+const ham = document.querySelector(".burger");
+const navi = document.querySelector(".navitem");
 let mbtn = false;
-ham.addEventListener('click', () =>{
-    if(!mbtn){
-        navi.classList.add('open');
-        ham.classList.add('open');
-        mbtn = true;
-    }else{
-        ham.classList.remove('open');
-        navi.classList.remove('open');
-        mbtn=false;
-    }
+ham.addEventListener("click", () => {
+  if (!mbtn) {
+    navi.classList.add("open");
+    ham.classList.add("open");
+    mbtn = true;
+  } else {
+    ham.classList.remove("open");
+    navi.classList.remove("open");
+    mbtn = false;
+  }
 });
